8028327c41658e1fd114918beada07a289eff0e3
Vickles <45949286+Vickwes@users.noreply.github.com>
2021-09-26 17:54:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AnimatedStatus
+ 
---
+++ main.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+
+const headers = {
+  "Content-Type": "application/json",
+  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+  "Authorization": "[EDIT THIS FILE AND ENTER TOKEN HERE]"
+}
+
+exampleFrames = [  //not used
+    "🔴",
+    "🟠",
+    "🟡",
+    "🟢",
+    "🔵",
+    "🟣"]
+    
+frames = [  //edit this to add different animations!  make sure any new lines are in quotes and have commas after them
+"a mimir 😴💤",
+"a mimir 😴　💤",
+"a mimir 😴　　💤",
+]
+
+
+
+const timer = ms => new Promise(res => setTimeout(res, ms))
+frame = 0;
+async function loop () { //shouldn't need to touch this
+  while(true) {
+    axios.patch("https://discord.com/api/v9/users/@me/settings", //discord pls give us api v9 doccumentation
+        {"custom_status":{"text":frames[frame],"expires_at":"2024-09-27T07:00:00.000Z"}},
+        {headers: headers})
+          .catch(error => {
+            console.error(error)
+          })
+    frame++;
+    frame = frame % frames.length;
+    await timer(4000); //any less than 4000 ms and rate limiting gets angry
+  }
+}
+
+loop();
